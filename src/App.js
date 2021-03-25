c75ae3b2f7ad495c174a36f443c2528aa38affa8
Manel-Hik <manelaloui2@gmail.com>
2021-03-25 16:42:03 +0100

    Checkpoint jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import myImage from "./imgsrc.jpg"
+import './style.css'
 
+const firstName = prompt("Who are you??");
 function App() {
+  
   return (
+    <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div style={{border:"solid 1px black",maxWidth:100}}>
+      <h1 className="title red"> Hello {firstName} </h1>
+      <br/>
+      <img src={myImage} alt ='myImage' />
+      <br/>
+      <img src="/arbre.jpeg" alt="myimage" />
+      </div>
       </header>
     </div>
+    <video width="750" height="500" controls >
+      <source src="/nature.mp4" type="video/mp4"/>
+     </video>
+    </>
   );
 }
 
